2340bd996d83f5dbb70b9aaee578f49d453f3e17
baturax <batuhanakkurt000@gmail.com>
2024-04-16 01:37:51 +0300

    sa

---
+++ installfont.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
+
+fontfolder = "myinstalledfonts"
+folder = "/usr/share/fonts/"
+
+os.system('clear')
+
+if fontfolder not in os.listdir('/usr/share/fonts'):
+    os.mkdir(f'{folder}{fontfolder}')
+
+def install_font():
+    selected_font = filedialog.askopenfilename(title='Select Font')
+    if selected_font:
+        font_name = os.path.basename(selected_font)
+        os.system(f'cp "{selected_font}" "{folder}{fontfolder}/{font_name}"')
+        os.system(f'fc-cache -f -v')
+        messagebox.showinfo('Success', 'Font installed successfully!')
+
+root = tk.Tk()
+root.title('Install Font')
+root.geometry('300x300')
+root.resizable(False, False)
+
+install_button = tk.Button(root, text='Install Font', command=install_font)
+install_button.pack()
+
+root.mainloop()
